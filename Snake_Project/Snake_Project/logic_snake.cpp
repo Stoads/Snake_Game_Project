7bd44a641cb55d74cc436b0a5aa25d31196217af
csjj1010 <csjj1010@naver.com>
2019-08-30 16:37:27 +0900

    snake update

--- Snake_Project/Snake_Project/logic_snake.cpp
+++ Snake_Project/Snake_Project/logic_snake.cpp
@@ -4,13 +4,25 @@
  */
 #include "logic.h"
 #include "code_data.h"
+int arr_x[4] = { 0,0,-1,1 };
+int arr_y[4] = { 1, -1, 0, 0 };
+int x, y;
 void move_snake() {
-
+	snake
 }
 bool is_snake_died() {
-
+	for (int i = 0; i < snake.size(); i++) {
+		if (snake[i] == Vec2(x, y))
+			return true;
+	}
 	return false;	//default
 }
-void head_turning(int direction) {
-	
+void head_turning(int key) {
+	switch (key) {
+	case UP: if(direction!=DOWN) direction = UP; break;
+	case RIGHT: if(direction != LEFT) direction = RIGHT; break;
+	case DOWN: if(direction != UP) direction = DOWN; break;
+	case LEFT: if(direction != RIGHT) direction = LEFT; break;
+	default: break;
+	}
 }
